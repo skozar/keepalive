eee1e1f4bb0050137d264237fc402e243509a54e
Kozar Sviatoslav <slav@kozar.xyz>
2026-07-22 14:29:13 -0600

    keepalive-ui v0.1.1: update formula

--- Formula/keepalive-ui.rb
+++ Formula/keepalive-ui.rb
@@ -2,7 +2,7 @@ class KeepaliveUi < Formula
   desc "Menu bar controller for the keepalive activity agent"
   homepage "https://github.com/skozar/keepalive"
   url "https://github.com/skozar/keepalive/releases/download/keepalive-ui-v0.1.0/Keepalive-0.1.0.zip"
-  sha256 "e2502d6e46367ff6d45c08fb18fb325efd867afa88bdfd914b96dc3f6ab5027f"
+  sha256 "e7756c44fb65f9a6cbce86cfbb4686907524370a75f1862a244f6e60c75a388a"
   license "MIT"
 
   depends_on "keepalive"
