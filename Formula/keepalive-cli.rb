b9fa96295d1ebe313dbb387d8d1f8923aa79a536
Sviatoslav Kozar <slav@kozar.xyz>
2026-07-27 15:00:30 -0600

    v0.11.2: update formulas

--- Formula/keepalive-cli.rb
+++ Formula/keepalive-cli.rb
@@ -1,9 +1,9 @@
 class KeepaliveCli < Formula
   desc "Keep macOS awake for Teams during chosen hours"
   homepage "https://github.com/skozar/keepalive"
-  version "0.11.1"
+  version "0.11.2"
   url "https://github.com/skozar/keepalive/releases/download/v#{version}/keepalive-cli-#{version}.tar.gz"
-  sha256 "164f8d6386df7069dfafccb02bfb58e2758a72c41a7d09afde2c8036cdc8abaf"
+  sha256 "f6f2b59e87d85d73c4609dbd2f2321354ac5494f415b618f77c54cc3eb88e370"
 
   def install
     libexec.install Dir["*"]
